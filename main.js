a0e405b013da9441871aba1bd284de2e1e9540bf
Razorwark09871 <111666061+Ninjagoboy@users.noreply.github.com>
2022-12-12 18:53:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload(){
+
+
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide()
+    poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses )  
+}
+function modelLoaded(){
+    console.log('poseNet is initianlized')
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+
+}
+function take_snapshot(){
+ save('myImage.png')   
+}  
+function gotPoses(Results){
+    if(Results.length > 0 ){
+        console.log(Results);
+        console.log("nose x = " + Results[0].pose.nose.x);
+        console.log("nose y = " + Results[0].pose.nose.y); 
+    }
+}
